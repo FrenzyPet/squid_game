0e8b944062df244a9ce61c37bba7855fd28d92e2
frenzy <manpatbro@gmail.com>
2023-08-02 22:17:38 +0300

    add Layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,11 @@
-const Layout = () => {
-  return <div></div>
+import { FC, ReactNode } from 'react'
+
+interface ILayoutProps {
+  children: ReactNode
+}
+
+const Layout: FC<ILayoutProps> = ({ children }) => {
+  return <div className="p-12 relative bg-cover bg-no-repeat">{children}</div>
 }
 
 export default Layout
